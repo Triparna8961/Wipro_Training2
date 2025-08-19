c8da47ea342dd7d611b0cd761772ec36c2648894
Triparna8961 <triparnasarkar0683@gmail.com>
2025-08-19 06:37:21 +0530

    Initial Commit

---
+++ springbootdemo/src/main/java/com/wipro/springbootdemo/respository/SEmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.wipro.springbootdemo.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wipro.springbootdemo.entity.SEmployee;
+
+@Repository
+public interface SEmployeeRepository extends JpaRepository<SEmployee, Long>, 
+PagingAndSortingRepository<SEmployee, Long>{
+	public SEmployee findByEmail(String email);
+	//public Page<SEmployee> findByEid(Pageable pageable);
+}
